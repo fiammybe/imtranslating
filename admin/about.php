8e47862daebd174a710d7dc76bf0fc81f237552e
David Janssens <david.j@impresscms.org>
2025-06-13 15:04:48 +0200

    Update about.php with IPF

--- admin/about.php
+++ admin/about.php
@@ -3,18 +3,12 @@
 /**
 * About page of the module
 *
-* @copyright	The SmartFactory http://www.smartfactory.ca
+* @copyright	fiammybe
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
-* @package		Ilog
-* @author		marcan <marcan@smartfactory.ca>
-* @version		$Id: about.php 1383 2008-05-21 17:17:43Z marcan $
+* @author		fiammybe <david.j@impresscms.org>
 */
 
-include_once("admin_header.php");
-
-include_once(ICMS_ROOT_PATH . "/kernel/icmsmoduleabout.php");
-$aboutObj = new IcmsModuleAbout();
+include_once "admin_header.php";
+$aboutObj = new icms_ipf_About();
 $aboutObj->render();
-
-?>
